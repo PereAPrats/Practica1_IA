7870cfad2bb63b48ac2a838bd62fe62f106bf074
PereAPrats <ppratsv03@gmail.com>
2024-10-23 16:54:55 +0200

    First Commit

---
+++ practica/__main__.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("/home/ppratsv03/UIB/3r/IA/practica/")
+
+from practica import agent, joc
+
+
+def main():
+    mida = (12, 12)
+
+    agents = [
+        agent.Viatger("Agent 1", mida_taulell=mida),
+        agent.Viatger("Agent 2", mida_taulell=mida),
+    ]
+
+    lab = joc.Laberint(agents, mida_taulell=mida)
+
+    lab.comencar()
+
+
+if __name__ == "__main__":
+    main()
